bd8291957c433f86a80c5773440abc29213c97ff
Annie Chhan <125323803+5ftsprout@users.noreply.github.com>
2024-05-24 22:45:08 -0400

    Added comments in server.js, index.ejs and main.js files.

--- public/js/main.js
+++ public/js/main.js
@@ -1,8 +1,11 @@
+//Declare variables from the document object model with element values
+//We are selecting classes
 const deleteBtn = document.querySelectorAll('.fa-trash')
 const item = document.querySelectorAll('.item span')
 const itemCompleted = document.querySelectorAll('.item span.completed')
-
+//Create array from the elements pulled from the DOM.
 Array.from(deleteBtn).forEach((element)=>{
+    //Give each element an event listener on click which calls the function in the second parameter.
     element.addEventListener('click', deleteItem)
 })
 
@@ -13,10 +16,14 @@ Array.from(item).forEach((element)=>{
 Array.from(itemCompleted).forEach((element)=>{
     element.addEventListener('click', markUnComplete)
 })
-
+//Follow functions allow the code to run asynchronously.
+//When the await keyword is utilised what follows will be sent to the task queue while the stack is able to continue its exectution.
 async function deleteItem(){
+    //itemText assigned the text appearing in the HTML of the element in the deleteBtn array.
     const itemText = this.parentNode.childNodes[1].innerText
     try{
+        //When this fetch is executed it is blocking the queue until finished and returned to the stack.
+        //We send a fetch request to '/deleteItem' which executes app.delete in our server.js file.
         const response = await fetch('deleteItem', {
             method: 'delete',
             headers: {'Content-Type': 'application/json'},
@@ -24,18 +31,23 @@ async function deleteItem(){
               'itemFromJS': itemText
             })
           })
+        //When this method call is executed it is blocking the queue until finished and returned to the stack.
+        //Console log our response from JSON and reload the document.
         const data = await response.json()
         console.log(data)
         location.reload()
-
+    //Catch errors executing the try statement.
     }catch(err){
         console.log(err)
     }
 }
 
 async function markComplete(){
+    //itemText assigned the text appearing in the HTML of the element in the item array.
     const itemText = this.parentNode.childNodes[1].innerText
     try{
+        //When this fetch is executed it is blocking the stack until finished.
+        //We send a fetch request to '/markComplete' which executes app.put in our server.js file.
         const response = await fetch('markComplete', {
             method: 'put',
             headers: {'Content-Type': 'application/json'},
@@ -43,18 +55,23 @@ async function markComplete(){
                 'itemFromJS': itemText
             })
           })
+        //When this method call is executed it is blocking the queue until finished and returned to the stack.
+        //Console log our response from JSON and reload the document.
         const data = await response.json()
         console.log(data)
         location.reload()
-
+    //Catch errors executing the try statement.
     }catch(err){
         console.log(err)
     }
 }
 
 async function markUnComplete(){
+    //itemText assigned the text appearing in the HTML of the element in the itemCompleted array.
     const itemText = this.parentNode.childNodes[1].innerText
     try{
+        //When this fetch is executed it is blocking the stack until finished.
+        //We send a fetch request to '/markComplete' which executes app.put in our server.js fi
         const response = await fetch('markUnComplete', {
             method: 'put',
             headers: {'Content-Type': 'application/json'},
@@ -62,10 +79,12 @@ async function markUnComplete(){
                 'itemFromJS': itemText
             })
           })
+        //When this method call is executed it is blocking the queue until finished and returned to the stack.
+        //Console log our response from JSON and reload the document.
         const data = await response.json()
         console.log(data)
         location.reload()
-
+    //Catch errors executing the try statement.
     }catch(err){
         console.log(err)
     }
--- server.js
+++ server.js
@@ -1,30 +1,50 @@
+//Import Express library that's accessed by variable app. 
+//Import MongoClient from MongoDB.
 const express = require('express')
 const app = express()
 const MongoClient = require('mongodb').MongoClient
+//Assign 2121 to PORT. Can be any port not already in use.
 const PORT = 2121
+//Import config from Dotenv library.
 require('dotenv').config()
 
-
+//Assign connection string from dotenv.
+//Our Mongo database name is 'todo'.
 let db,
     dbConnectionStr = process.env.DB_STRING,
     dbName = 'todo'
 
+//Connect to 'todo' database using the connection string.
+//Of the 7 topology classes, we are using Unified Topology:
+// - fully support Server Discover and Monitoring, Server Selection and Max Staleness specs
+// - reduce maintenance, model all supported topolgy types with a single engine
+// - remove confusing functionality
 MongoClient.connect(dbConnectionStr, { useUnifiedTopology: true })
     .then(client => {
         console.log(`Connected to ${dbName} Database`)
         db = client.db(dbName)
     })
     
+//The view engine/HTML template we are using is EJS.
 app.set('view engine', 'ejs')
+//Default folder to access static resources (imgs, js, css, etc) is 'public'
 app.use(express.static('public'))
+//Recognise requested objects as strings or arrays.
 app.use(express.urlencoded({ extended: true }))
+//Recognise requested JSON objects.
 app.use(express.json())
 
-
+//GET request from path '/'
+//Asynchronously execute the callback function.
 app.get('/',async (request, response)=>{
+    //Await variable values to be assigned from the database before continuing.
+    //todoItems holds an array.
+    //itemsLeft will hold a number representing the amount of objects with the attribute completed with the value false.
     const todoItems = await db.collection('todos').find().toArray()
     const itemsLeft = await db.collection('todos').countDocuments({completed: false})
+    //Render our HTML template file. Local variables (item, left) are assigned values for the view (HTML template file).
     response.render('index.ejs', { items: todoItems, left: itemsLeft })
+    //Code below uses promises with .then and .catch handlers instead of async/await above.
     // db.collection('todos').find().toArray()
     // .then(data => {
     //     db.collection('todos').countDocuments({completed: false})
@@ -35,8 +55,11 @@ app.get('/',async (request, response)=>{
     // .catch(error => console.error(error))
 })
 
+//POST response to path '/addTodo'
 app.post('/addTodo', (request, response) => {
+    //Insert an object into the database. Variables (thing, completed) are assigned values. Requesting from the body (main content, index.ejs).
     db.collection('todos').insertOne({thing: request.body.todoItem, completed: false})
+    //Then handler redirect the user back to path '/'. Catch errors
     .then(result => {
         console.log('Todo Added')
         response.redirect('/')
@@ -44,15 +67,21 @@ app.post('/addTodo', (request, response) => {
     .catch(error => console.error(error))
 })
 
+//UPDATE response to path '/markComplete'
 app.put('/markComplete', (request, response) => {
+    //Update database with request from the body (main content, main.js)
     db.collection('todos').updateOne({thing: request.body.itemFromJS},{
+        //Replace the value of variables (completed).
         $set: {
             completed: true
           }
     },{
+        //Sort by _id in descending order.
         sort: {_id: -1},
+        //Update and Insert: when false does not insert a new document when no match is found.
         upsert: false
     })
+    //Send 'Marked Complete' in response. Catch errors.
     .then(result => {
         console.log('Marked Complete')
         response.json('Marked Complete')
@@ -61,15 +90,21 @@ app.put('/markComplete', (request, response) => {
 
 })
 
+//UPDATE response to path '/markUnComplete'
 app.put('/markUnComplete', (request, response) => {
+    //Update database with request from the body (main content, main.js)
     db.collection('todos').updateOne({thing: request.body.itemFromJS},{
+        //Replace the value of variables (completed).
         $set: {
             completed: false
           }
     },{
+        //Sort by _id in descending order.
         sort: {_id: -1},
+         //Update and Insert: when false does not insert a new document when no match is found.
         upsert: false
     })
+    //Send 'Marked Complete' in response. Catch errors.
     .then(result => {
         console.log('Marked Complete')
         response.json('Marked Complete')
@@ -78,8 +113,11 @@ app.put('/markUnComplete', (request, response) => {
 
 })
 
+//DELETE in response to path '/deleteItem'
 app.delete('/deleteItem', (request, response) => {
+    //Delete from database the requested data from body (main content, main.js)
     db.collection('todos').deleteOne({thing: request.body.itemFromJS})
+    //Send 'Todo Deleted' in response. Catch errors.
     .then(result => {
         console.log('Todo Deleted')
         response.json('Todo Deleted')
@@ -88,6 +126,8 @@ app.delete('/deleteItem', (request, response) => {
 
 })
 
+//To access what is being served, and send requests we must go to where the server is listening.
+//We will try the port supplied by dotenv, otherwise we are sent to port 2121.
 app.listen(process.env.PORT || PORT, ()=>{
     console.log(`Server running on port ${PORT}`)
 })
